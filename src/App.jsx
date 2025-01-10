6342d7c41977a835c36bf0b1d266fa64261a2469
Тахмина Калмамат кызы <tahmina1412@icloud.com>
2025-01-11 02:36:28 +0300

    create ui comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { Comments } from "./components/UI/comments/Comments";
-
 export const App = () => {
   return (
     <div>
       <h1>Task tracker</h1>
-      <Comments/>
     </div>
   );
 };
