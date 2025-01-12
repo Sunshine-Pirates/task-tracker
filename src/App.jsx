95b74fea28eee135dc89e8f099c3b2d1b0748e58
Тахмина Калмамат кызы <tahmina1412@icloud.com>
2025-01-13 01:39:31 +0300

    fixed favorites

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { Input } from "./components/UI/input/Input";
-import { SearchInput } from "./components/UI/searchInput/SearchInput";
-
 export const App = () => {
   return (
     <div>
       <h1>Task tracker</h1>
-      <SearchInput />
-      <Input type="password" />
     </div>
   );
 };
