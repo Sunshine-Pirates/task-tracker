1ebb52638e44f07ea1dbdb84264a02e31a393288
AidanaKalilKyzy <aidana6670@gmail.com>
2025-01-13 22:24:29 +0300

    createBoards

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { Notification } from "./notification/notification";
+
 export const App = () => {
   return (
     <div>
       <h1>Task tracker</h1>
+      <Notification />
     </div>
   );
 };
