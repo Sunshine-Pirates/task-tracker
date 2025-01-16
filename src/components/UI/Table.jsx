583dbef1f983d74333c76f614ffada6f3f21cf25
Alisher-00kg <alisher.estebesov@gmail.com>
2025-01-16 17:54:34 +0300

    save

--- src/components/UI/Table.jsx
+++ src/components/UI/Table.jsx
@@ -1,3 +1,63 @@
+import { useTable } from "react-table";
+import {
+  Paper,
+  styled,
+  Table as MuiTable,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { userTable } from "../../utils/constants/userTable";
+
 export const Table = () => {
-  return <div>Table</div>;
+  return (
+    <StyledMainContainer>
+      <TableContainer component={Paper}>
+        Workspaces
+        <MuiTable>
+          <TableHead>
+            <TableRow>
+              <TableCell>№</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Lead</TableCell>
+              <TableCell>Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {userTable.map((row, index) => (
+              <TableRow
+                key={row.id}
+                style={{
+                  backgroundColor: index % 2 === 0 ? "#FFFFFF" : "#F3F3F3",
+                }}
+              >
+                <TableCell>{row.number}</TableCell>
+                <TableCell>{row.workspaceTitle}</TableCell>
+                <TableCell>{row.lead}</TableCell>
+                <TableCell>
+                  <img
+                    src={row.userIcon}
+                    alt=""
+                    style={{
+                      width: "40px",
+                      height: "40px",
+                      objectFit: "cover",
+                    }}
+                  />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </MuiTable>
+      </TableContainer>
+    </StyledMainContainer>
+  );
 };
+
+const StyledMainContainer = styled("div")(() => ({
+  width: "100%",
+  height: "100%",
+  padding: "16px 40px 10px 40px",
+}));
---
+++ src/utils/constants/userTable.js
@@ -0,0 +1,30 @@
+export const userTable = [
+  {
+    id: 11,
+    number: 1,
+    workspaceTitle: "Taigan",
+    userIcon: "user.png",
+    lead: "Almaz Almazov",
+  },
+  {
+    id: 12,
+    number: 2,
+    workspaceTitle: "Shoppix",
+    userIcon: "user.png",
+    lead: "Almaz Almazov",
+  },
+  {
+    id: 13,
+    number: 3,
+    workspaceTitle: "Task traker",
+    userIcon: "user.png",
+    lead: "Almaz Almazov",
+  },
+  {
+    id: 14,
+    number: 4,
+    workspaceTitle: "Mobile UX-UI",
+    userIcon: "user.png",
+    lead: "Almaz Almazov",
+  },
+];
