0a252513e3e560dffd0feed549458dac20a143ab
Alisher-00kg <alisher.estebesov@gmail.com>
2025-01-23 15:56:04 +0300

    save changes and create new function handleOpenOrCloseModal in FilterModal component

---
+++ src/components/UI/CreateNewWorkspace.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { BaseModal } from "./BaseModal";
+
+export const CreateNewWorkspace = () => {
+  //   const [open, setOpen] = useState(false);
+  <>
+    <button>open create new workspace</button>
+    return <BaseModal>CreateNewWorkspace</BaseModal>;
+  </>;
+};
