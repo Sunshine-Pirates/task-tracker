f4f4e2311c1f43077fbf6f3434f1c7b58b58c1c5
AidanaKalilKyzy <aidana6670@gmail.com>
2025-01-27 02:00:40 +0300

    resolved-commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { NotificationComponent } from "./notification/NotificationComponent";
 
 export const App = () => {
   return (
     <div>
       <h1>Task tracker</h1>
-      <NotificationComponent />
     </div>
   );
 };
