c93f1e3c30ada2ca735cd90c675d08932f2a2fd7
Alisher-00kg <alisher.estebesov@gmail.com>
2025-02-07 18:42:49 +0300

    create component

--- src/components/UI/CreateNewWorkspace.jsx
+++ src/components/UI/CreateNewWorkspace.jsx
@@ -2,9 +2,21 @@ import { useState } from "react";
 import { BaseModal } from "./BaseModal";
 
 export const CreateNewWorkspace = () => {
-  //   const [open, setOpen] = useState(false);
-  <>
-    <button>open create new workspace</button>
-    return <BaseModal>CreateNewWorkspace</BaseModal>;
-  </>;
+  const [open, setOpen] = useState(false);
+  const handleOpenOrCloseModal = () => {
+    setOpen((prev) => !prev);
+  };
+  return (
+    <>
+      <button onClick={handleOpenOrCloseModal}>
+        open create new workspace
+      </button>
+      <BaseModal open={open} onClose={handleOpenOrCloseModal}>
+        <header>
+          <p>Create a new workspace</p>
+        </header>
+        <main></main>
+      </BaseModal>
+    </>
+  );
 };
